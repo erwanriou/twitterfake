f0f28442b156dff42f2740ddcd0fcf18c41ee952
Erwan Riou <rr.1.riou@gmail.com>
2018-05-25 17:04:14 +0200

    implementing middleware functions

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css'
 import App from './components/App'
 import reducer from './reducers'
+import middleware from './middleware'
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(reducer, composeWithDevTools(middleware));
 
 ReactDOM.render(
   <Provider store={store}>
